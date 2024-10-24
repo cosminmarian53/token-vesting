0c1905cd1d14f0c69cfa481f576918f2c9c10fdd
cosminmarian53 <cosmin.lavric@student.unitbv.ro>
2024-10-24 23:41:21 +0300

    First intruction done-create_Vesting_account

--- anchor/programs/tokenvesting/src/lib.rs
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -1,70 +1,86 @@
 #![allow(clippy::result_large_err)]
-
+// Import the necessary modules
 use anchor_lang::prelude::*;
-
+use anchor_spl::token_interface::*;
+// Declare the program's ID
 declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
-
+// Define the program
 #[program]
 pub mod tokenvesting {
     use super::*;
+    // Define the create_vesting_account function
+    pub fn create_vesting_account(
+        ctx: Context<CreateVestingAccount>,
+        company_name: String,
+    ) -> Result<()> {
+        *ctx.accounts.vesting_account = VestingAccount {
+            owner: ctx.accounts.signer.key(),
+            mint: ctx.accounts.mint.key(),
+            treasury_token_account: ctx.accounts.treasury_token_account.key(),
+            company_name,
+            treasury_bump: ctx.bumps.treasury_token_account,
+            bump: ctx.bumps.vesting_account,
+        };
 
-  pub fn close(_ctx: Context<CloseTokenvesting>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.tokenvesting.count = ctx.accounts.tokenvesting.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.tokenvesting.count = ctx.accounts.tokenvesting.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeTokenvesting>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.tokenvesting.count = value.clone();
-    Ok(())
-  }
+        Ok(())
+    }
 }
-
+// Create the CreateVestingAccount struct, which will be used to create a new vesting account
+// The struct has two fields: signer and vesting_account
+// The signer field is a Signer object, which is used to sign the transaction
+// The vesting_account field is an Account object, which is used to store the vesting account data
+// The struct is annotated with the Accounts attribute, which specifies the accounts that will be used in the function
 #[derive(Accounts)]
-pub struct InitializeTokenvesting<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+#[instruction(company_name: String)]
+pub struct CreateVestingAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    // The vesting_account field is annotated with the init attribute, which specifies that the account will be initialized
+    // The payer field specifies the account that will pay for the transaction
+    // The space field specifies the size of the account data
+    // The seeds field specifies the seeds that will be used to derive the account's address
+    // The bump field specifies the bump seed that will be used to derive the account's address
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + VestingAccount::INIT_SPACE,
+        seeds = [company_name.as_ref()],
+        bump,
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+    pub mint: InterfaceAccount<'info, Mint>,
 
-  #[account(
-  init,
-  space = 8 + Tokenvesting::INIT_SPACE,
-  payer = payer
-  )]
-  pub tokenvesting: Account<'info, Tokenvesting>,
-  pub system_program: Program<'info, System>,
+    #[account(init,
+        token::mint = mint,
+        token::authority = treasury_token_account,
+        payer = signer,
+        seeds = [b"vesting", company_name.as_bytes()],
+        bump
+    )]
+    pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
 }
-#[derive(Accounts)]
-pub struct CloseTokenvesting<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub tokenvesting: Account<'info, Tokenvesting>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub tokenvesting: Account<'info, Tokenvesting>,
-}
-
+// Define the VestingAccount struct, which will be used to store the vesting account data
+// The struct has six fields: owner, mint, treasury_token_account, company_name, treasury_bump, and bump
+// The owner field is a Pubkey object, which specifies the owner of the vesting account
+// The mint field is a Pubkey object, which specifies the mint of the vesting account
+// The treasury_token_account field is a Pubkey object, which specifies the treasury token account of the vesting account
+// The company_name field is a String object, which specifies the company name of the vesting account
+// The treasury_bump field is a u8 object, which specifies the treasury bump of the vesting account
+// The bump field is a u8 object, which specifies the bump of the vesting account
+// The struct is annotated with the Account attribute, which specifies that the struct will be used as an account
 #[account]
 #[derive(InitSpace)]
-pub struct Tokenvesting {
-  count: u8,
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub treasury_token_account: Pubkey,
+
+    #[max_len(50)]
+    pub company_name: String,
+
+    pub treasury_bump: u8,
+    pub bump: u8,
 }
